4ef8251b70a2ded2ae3475f89b0739ec68475c3a
Salma ElMahy <97133077+elmahygurl@users.noreply.github.com>
2023-12-22 05:19:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 #include <signal.h>
 #include <sys/time.h>
 
-#define N 9
-#define t1 0.5
+#define N 9  //number of counter threads 
+//#define t1 1 //time interval for monitoring
+#define b 10 //byffer size
+
 //ANSI escape codes for colorful console output
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -17,6 +19,81 @@
 
 int count=0;  //accessed by mMonitor and incremented by mCounters
 sem_t  semCounter;
+sem_t semBuffer;
+int buffer[b];
+
+int front = 0;
+int rear = 0;
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void enqueue(int value)
+{    
+    printf("\n%sWaiting to enter ENQUEUE critical section to write in buffer\n",KRED);
+    sem_wait(&semBuffer);
+    printf("\n%sIN CRITICAL SECTION of enqueue- writing in buffer\n",KRED);
+    if ((rear + 1) % b == front) //full buffer -no place to write
+    {       
+        printf("\n%sMonitor thread: Buffer full!! DIscarding value=%d\n",KRED, value);
+        sem_post(&semBuffer);
+        return;
+    }
+
+    buffer[rear] = value;
+    printf("\n%sExiting enqueue critical section after enqueing at index=%d\n",KRED,rear);
+    rear = (rear + 1) % b;
+    
+    
+    sem_post(&semBuffer);
+}
+
+
+int dequeue()
+{
+    printf("\n%sWaiting to enter critical section to read from buffer-dequeue\n",KYEL);
+    sem_wait(&semBuffer);
+    printf("\n%sIN CRITICAL SECTION of dequeue- reading from buffer\n",KYEL);
+    
+    if (front == rear)  //empty buffer - nothing to read
+    {	
+    	printf("\n%sCollector thread: nothing is in the buffer!",KYEL);
+        sem_post(&semBuffer);
+        return -1; 
+    }
+
+    int value = buffer[front];
+    printf("\n%sCollector thread: reading from the buffer at position %d",KYEL,front);
+    front = (front + 1) % b;
+    printf("\n%sExiting dequeue critical section\n",KYEL);
+    sem_post(&semBuffer);
+    return value;
+}
+
+
+
+
+void* collectingg(void *args)  
+{
+    while (1)
+    {
+    	int randomProcessingTime = rand() % 3 + 1; //get random num between 1 and 3
+        sleep(randomProcessingTime);
+        int value = dequeue();         //dequeue values from buffer 
+        if (value != -1)
+        {
+            printf("\n%s mCollector dequeued value: %d\n", KYEL, value);
+        }
+        
+        //sleep(t1); //every time interval
+    }
+}
+
+
 
 void* countingg(void *args)
 {
@@ -26,7 +103,7 @@ void* countingg(void *args)
     printf("\n%sDa5lna critical of mCOunter no %d\n", KYEL,thread_no);
     count++;
     printf("\n%s mCounter no %d exiting critical section with count=%d\n",KGRN,thread_no,count);
-    free(args); // Free the allocated memory
+    free(args); 
     sem_post(&semCounter);
 
 }
@@ -34,25 +111,34 @@ void* countingg(void *args)
 
 void* monitoringg(void *args)
 {
-
-	while(1){  	
-	printf("\n%smMonitor tryna enter critical section\n",KBLU);
+	while(1){ 
+	int t1 = rand() % 2 + 1; //get random num between 1 and 2
+        sleep(t1); 	
+	printf("\n%sMonitor thread: waiting to read counter\n",KBLU);
+	
     	sem_wait(&semCounter); // Lock the counter
-    	printf("\n%sDa5lna critical of mMonitor\n", KYEL);
+    	printf("\n%sDa5lna critical of mMonitor\n", KBLU);
     	int current_count = count;
-    	count= 0; // Reset the counter
-    	printf("\n%sExiting critical section of mMonitor, count before=%d\n", KBLU,current_count);
-    	sem_post(&semCounter); // Unlock the counter
-	sleep(t1); 
- }
+    	count= 0; // reset count
+    	
+
+    	printf("\n%sMonitor thread: reading a count value of %d\n", KBLU,current_count); //exiting critical 
+    	sem_post(&semCounter); // unlock the counter
+    	
+    	enqueue(current_count);  //into buffer 
+    	printf("\n%sEnqued into buffer count=%d\n",KRED,current_count);
+    	
+	
+ 	}
 }
 
 void intHandler(int dummy)
 {
     // set the noramal color back
     printf("%sExit\n", KNRM);
-    //destroy the semaphore
+    //destroy semaphores
     sem_destroy(&semCounter);
+    sem_destroy(&semBuffer);
     exit(0);
 }
 
@@ -62,27 +148,34 @@ int main()
     signal(SIGINT, intHandler);
     pthread_t mMonitor, mCollector;
     pthread_t mCounter[N] ;
+    
     sem_init(&semCounter, 0, 1);
+    sem_init(&semBuffer, 0, 1);
     
     pthread_create(&mMonitor, NULL, monitoringg, NULL); //monitoring
     
+    pthread_create(&mCollector, NULL, collectingg, NULL); 
+    
+    
     while(1){
-		//sleep(0.9);
+    	
         for (int i=0; i<N; i++)
     	{
+    	int r=rand()%3+1;
+    	printf("random counter var sleep = %d",r);
+	sleep(r);
 		int *thread_no = malloc(sizeof(int));
        		*thread_no = i;
        		pthread_create(&mCounter[i],NULL,countingg, thread_no);
     	}
-   
-
+  
    	for (int i = 0; i < N; i++)
     	{
         pthread_join(mCounter[i], NULL); // Wait for mCounter threads to finish
     	}
   
  	}
- 	//pthread_join(mMonitor, NULL); 
+ 	
     return 0;
 
 }
